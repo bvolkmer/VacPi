294ad3da52e060b757aabaa8243f74152014324c
Benedikt Volkmer <bvolkmer@users.noreply.github.com>
2018-03-13 17:07:22 +0000

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -18,10 +18,10 @@ wget "$ARDUINO_URL" -O "$ARDUINO_FILE"
 tar -xJf "$ARDUINO_FILE"
 sudo mv "$ARDUINO_BASENAME/" "$ARDUINO"
 
-echo "Installing avr-gcc from Arduino 1.8.5..."
+echo "Installing avr-gcc from Arduino 1.5.5..."
 
-#wget "$AVR_GCC_URL" -O "$AVR_GCC_FILE"
-#tar -xzf "$AVR_GCC_FILE"
+wget "$AVR_GCC_URL" -O "$AVR_GCC_FILE"
+tar -xJf "$AVR_GCC_FILE"
 sudo mv "$AVR_GCC_BASENAME/hardware/tools/avr" "$AVR_GCC"
 
 echo "Installation of dependencies is complete, we are now going to run some tests..."
